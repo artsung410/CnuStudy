6bb548ad9fdc9644e15925dda84a4709d0dd87d2
usrrt <gkenfktm@gmail.com>
2022-05-13 12:09:46 +0900

    Feat : struct and change totaldeltatime name

--- framworkbuild/framworkbuild/shootTheBullet.c
+++ framworkbuild/framworkbuild/shootTheBullet.c
@@ -15,54 +15,85 @@ struct Bullet
 {
 	int posX;
 	int posY;
+	float aliveTime;
+	bool shoot;
 	bool alive;
 
 }bullet;
 
 int main(void)
 {
-	clock_t prevTic = clock(); //한번만실행
-	float totalDeltaTime = 0;
+	clock_t prevTic = clock();
 
 	while (1)
 	{
 		system("cls");
-
+		/*char map[30][120] = {""};
+		for (int i = 0; i < 30; i++)
+		{
+			for (int j = 0; j < 120; j++)
+			{
+				map[i][j] = '#';
+				printf("%c", map[i][j]);
+			}
+				printf("\n");
+		}*/
 #pragma region 입력
+
 		//입력 : GetAsyncKeyState()
 		if (GetAsyncKeyState(VK_DOWN))
 		{
 			++player.posY;
+			if (player.posY > 30)
+			{
+				--player.posY;
+			}
 		}
+
 		if (GetAsyncKeyState(VK_UP))
 		{
 			--player.posY;
+			if (player.posY < 0)
+			{
+				++player.posY;
+			}
 		}
+
 		if (GetAsyncKeyState(VK_LEFT))
 		{
 			--player.posX;
+			if (player.posX < 0)
+			{
+				++player.posX;
+			}
 		}
+
 		if (GetAsyncKeyState(VK_RIGHT))
 		{
 			++player.posX;
+			if (player.posX > 100)
+			{
+				--player.posX;
+			}
+		}
+
+		if (GetAsyncKeyState(VK_SPACE) & 0x0001 && bullet.alive == false)
+		{
+			bullet.shoot = true;
 		}
 
 #pragma endregion
 
 #pragma region 처리?
 
-		//움직일 문자
-		COORD playerPos = { player.posX, player.posY };
-		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), playerPos);
-		printf("P");
-
-		COORD bulletPos = { bullet.posX, bullet.posY };						// 여기가 총알 초기 좌표
-		if (GetAsyncKeyState(VK_SPACE) & 0x0001 && bullet.alive == false)	// 스페이스바 입력받음 && 총알 나가는 동안 입력 안받음
+		COORD bulletPos = { bullet.posX, bullet.posY };						
+		if (bullet.shoot)
 		{
-			bullet.posX = player.posX + 1;									// 총알은 플레이어 앞에서 나간다 여기가 총알 시작위치
-			bullet.posY = player.posY;										// y좌표에 플레이어 좌표 표시해줌 
-			SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), bulletPos);
-			bullet.alive = !bullet.alive;									// 스페이스바 누르면 bool값이 true로 바뀐다
+			bullet.posX = player.posX + 1;								
+			bullet.posY = player.posY;										
+			//SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), bulletPos);
+			bullet.alive = true;											
+			bullet.shoot = false;
 		}
 
 		// 델타타임 구현
@@ -91,30 +122,36 @@ int main(void)
 
 */
 
-
-		//델타타임 더해주는 변수추가
-		totalDeltaTime += deltaTime;
-		if (totalDeltaTime > 3.0f)
+		if (bullet.alive == true)
 		{
-			bullet.alive = false;
-			totalDeltaTime = 0; // 다시 0으로 초기화
+			//델타타임 더해주는 변수추가
+			bullet.posX++;													
+			bullet.aliveTime += deltaTime;
+			if (bullet.aliveTime > 3.0f)
+			{
+				bullet.alive = false;
+				bullet.aliveTime = 0;
+			}
 		}
+
 #pragma endregion
 
 #pragma region 출력
 
-		if (bullet.alive)													// true로 바뀌면 여기가 실행되고
+		//움직일 문자
+		COORD playerPos = { player.posX, player.posY };
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), playerPos); 
+		printf("P");
+
+		if (bullet.alive)													
 		{
-			bullet.posX++;													// 여기가 x좌표가 증가하는부분이고 <- 얘가 계속 증가댐
-			SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), bulletPos); // 증가된 좌표를 여기서 다시 표시해줌
-			printf("O");													// 좌표가 변화될때마다 총알 출력
+			SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), bulletPos); 
+			printf("총알~");													
 		}
 
 #pragma endregion
 
-
 		
-
 		// 총알의 속성
 		// 모양
 		// 좌표

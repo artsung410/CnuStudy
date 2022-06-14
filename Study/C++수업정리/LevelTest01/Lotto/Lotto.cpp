ed831e2a65940786b4b253a655fc35a0187a2486
SungKwonMoon <artsung410@gamil.com>
2022-06-14 23:02:07 +0900

    0614_1

--- Study/C++수업정리/LevelTest01/Lotto/Lotto.cpp
+++ Study/C++수업정리/LevelTest01/Lotto/Lotto.cpp
@@ -1,20 +1,76 @@
-﻿// Lotto.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
 
-#include <iostream>
+using namespace std;
+
+/// <summary>
+/// 로또 번호를 만든다.
+/// </summary>
+/// <param name="maxValue">최대 번호값</param>
+/// <param name="digitCount">생성 숫자 개수</param>
+/// <returns>로또 번호들 (동적 할당된) </returns>
+int* MakeLotto(int maxValue, int digitCount);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// 입력 : 최대 번호값과 생성 숫자 개수 입력 받는다.
+	cout << "최대 번호 값을 입력하세요. : ";
+	int maxValue;
+	cin >> maxValue;
+
+	cout << "생성할 숫자의 개수를 입력하세요. : ";
+	int digitCount;
+	cin >> digitCount;
+
+	// 처리 : 로또 생성 
+	int* lotto = MakeLotto(maxValue, digitCount); // 생성? 생성자
+
+	// 출력 : 로또 번호 출력
+	for (int i = 0; i < digitCount; ++i)
+	{
+		cout << lotto[i] << " ";
+	}
+
+	delete[] lotto;
+
+	return 0;
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+int* MakeLotto(int maxValue, int digitCount)
+{
+	// 1 ~ maxValue 사이의 값 중 중복되지 않게 digitCount만큼 뽑음.
+	int* lotto = new int[digitCount];
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+	srand(time(nullptr));
+
+	// 로또 번호 정하기
+	// 3. 1번부터 digitCount만큼 반복한다.
+	for (int curPos = 0; curPos < digitCount; ++curPos)
+	{
+		int candidate = 0;
+		bool isExist = false;
+
+		do
+		{
+			// 1. [1, maxValue] 사이의 값으로 무작위로 뽑는다.
+			candidate = 1 + rand() % maxValue;
+			isExist = false;
+
+			// 2. 중복인지 확인한다. => 이미 나온 값인지 확인한다.
+			for (int i = 0; i < curPos; ++i)
+			{
+				// 2-1. 중복이면? 1번을 다시 수행한다.
+				if (candidate == lotto[i])
+				{
+					isExist = true;
+
+					break;
+				}
+			}
+		} while (isExist);
+
+		// 2-2. 중복이 아니면? 데이터를 저장한다.
+		lotto[curPos] = candidate;
+	}
+
+	return lotto;
+}

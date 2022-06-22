ded65ccf458d225339222d16ab97dde3a20dc238
sung kwon moon <artsung410@gmail.com>
2022-06-22 20:39:35 +0900

    0622

--- Study/C++수업정리/Stack/Stack/stack4949/stack4949.cpp
+++ Study/C++수업정리/Stack/Stack/stack4949/stack4949.cpp
@@ -1,9 +1,14 @@
 ﻿#include <iostream>
 #include <stack>
+#include <string>
 
 using namespace std;
 
-int BracketCnt[2] = { 0 };
+
+int bracketCnt_S = 0;
+int bracketCnt_L = 0;
+
+bool isBracketClose = false;
 
 int main()
 {
@@ -12,16 +17,77 @@ int main()
     cout.tie(nullptr);
 
     std::stack<char> s;
-    int size = 0;
 
     while (1)
     {
         // 1. 입력
         string Brackets;
-        cin >> Brackets;
+        getline(cin, Brackets);
+
+        // 2. 스택에 요소를 담는다.
+        for (int i = 0; i < Brackets.size(); i++)
+        {
+            s.push(Brackets[i]);
+        }
+
+        int size = 0;
+
+        // 2. 처리
+        for (int i = 0; i < size; i++)
+        {
+            if (s.top() == '(')
+            {
+                if (bracketCnt_S)
+                {
+                    --bracketCnt_S;
+
+                    if (bracketCnt_S < 0)
+                    {
+                        cout << "NO" << "\n";
+                        isBracketClose = true;
+                        break;
+                    }
+
+                }
+            }
+
+            else if (s.top() == ')')
+            {
+                ++bracketCnt_S;
+            }
+
+            else if (s.top() == '[')
+            {
+                if (bracketCnt_S)
+                {
+                    --bracketCnt_L;
+
+                    if (bracketCnt_S < 0)
+                    {
+                        cout << "NO" << "\n";
+                        isBracketClose = true;
+                        break;
+                    }
+                }
+            }
+
+            else if (s.top() == ']')
+            {
+                ++bracketCnt_L;
+            }
+            s.pop();
+        }
+        // ####### 처리부 종료 ###########
+
+        // 3. 출력
+
+        if (!isBracketClose)
+        {
+
+        }
 
     }
 
-    int size = 0;
 
+    return 0;
 }

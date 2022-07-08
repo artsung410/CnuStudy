c7e36ac0bc166acf063b2d45f04370ab8bc4ed3e
usrrt <gkenfktm@gmail.com>
2022-07-08 22:54:56 +0900

    Check

---
+++ Baekjoon/Baekjoon/스택/17298_오큰수.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+#define MAX_SIZE 1'000'005
+
+stack<int> st;
+int N;
+int seq[MAX_SIZE];
+
+int main()
+{
+	cin >> N;
+}
+
+/*
+
+수열의 각 원소 Ai에 대한 오큰수 NGE(i)
+오큰수는 오른쪽에 있으면서, Ai보다 큰 수 중에서 가장 왼쪽에 있는 수
+리ㅏㅇ;ㅓㄴ마리;ㅓㅏ
+
+*/
